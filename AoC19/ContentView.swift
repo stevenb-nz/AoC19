5644cfa50e6fda96c361749a394977fde1bf6bec
Steven Brown <sgbrown@mac.com>
2019-11-21 05:19:17 +1300

    Initial Commit

---
+++ AoC19/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AoC19
+//
+//  Created by Steven Brown on 21/11/19.
+//  Copyright © 2019 Steven Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
